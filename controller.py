019acc7c83665875bb7ee5690ed67e967abf4d3a
Jordan <Jordanshaqallen@gmail.com>
2022-11-08 17:40:42 +0800

    commit for heroku

--- controller.py
+++ controller.py
@@ -1,9 +1,12 @@
 import psycopg2
+import os
 from flask import Flask, render_template, request, redirect
+
+DB_URL = ox.environ.get('DATABASE_URL', 'dbname=colorpicker')
 
 def insert_data(query, params = []):
 
-    conn = psycopg2.connect('dbname=colorpicker')
+    conn = psycopg2.connect('DB_URL')
     cur = conn.cursor()
     cur.execute(query, params)
     conn.commit()
@@ -11,7 +14,7 @@ def insert_data(query, params = []):
 
 def sql_select1(query, params = []):
 
-    conn = psycopg2.connect('dbname=colorpicker')
+    conn = psycopg2.connect('DB_URL')
     cur = conn.cursor()
     cur.execute(query, params)
     row = cur.fetchone()
@@ -19,7 +22,7 @@ def sql_select1(query, params = []):
 
 def sql_select(query, params = []):
 
-    conn = psycopg2.connect('dbname=colorpicker')
+    conn = psycopg2.connect('DB_URL')
     cur = conn.cursor()
     cur.execute(query, params)
     return cur.fetchall()
@@ -27,7 +30,7 @@ def sql_select(query, params = []):
 
 
 def delete_pallet(name, id):
-    conn = psycopg2.connect('dbname=colorpicker')
+    conn = psycopg2.connect('DB_URL')
     cur = conn.cursor()
     cur.execute(f'DELETE FROM saved_pallets WHERE (name = %s AND user_id = %s)', [name, id])
     conn.commit()
